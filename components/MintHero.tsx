286df94177c280d3f105ef9afcbbe7cdc4543c5b
gokaiorg <jeremydouchamps@gmail.com>
2023-12-24 05:37:04 +0700

    Update drop 4 coming soon

--- components/MintHero.tsx
+++ components/MintHero.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Box, Text, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 // import Link from 'next/link';
-import { useCallback, useEffect } from 'react';
-import { Address } from '@multiversx/sdk-core';
-import { SCQueryType, useAccount } from '@useelven/core';
+// import { useCallback, useEffect } from 'react';
+// import { Address } from '@multiversx/sdk-core';
+import { SCQueryType } from '@useelven/core';
 import { useElvenScQuery } from '../hooks/useElvenScQuery';
 // import { MintForm } from './MintForm';
 // import { Authenticated } from './core/Authenticated';
@@ -16,178 +16,178 @@ import { useElvenScQuery } from '../hooks/useElvenScQuery';
 // TODO: Prepare separate components for the segments here
 
 export const MintHero = () => {
-  const { address } = useAccount();
-  // const { chainType } = useConfig();
+  // const { address } = useAccount();
+  // // const { chainType } = useConfig();
 
   const { data: mintingPaused } = useElvenScQuery<boolean>({
     funcName: 'isMintingPaused',
     type: SCQueryType.BOOLEAN,
   });
 
-  const { data: dropActive, fetch: refreshDropState } =
-    useElvenScQuery<boolean>({
-      funcName: 'isDropActive',
-      type: SCQueryType.BOOLEAN,
-      autoInit: !mintingPaused,
-    });
+  // const { data: dropActive, fetch: refreshDropState } =
+  //   useElvenScQuery<boolean>({
+  //     funcName: 'isDropActive',
+  //     type: SCQueryType.BOOLEAN,
+  //     autoInit: !mintingPaused,
+  //   });
 
-  const {
-    data: allowlistEnabled,
-    fetch: refreshAllowlistState,
-    isLoading: allowlistStateLoading,
-  } = useElvenScQuery<boolean>({
-    funcName: 'isAllowlistEnabled',
-    type: SCQueryType.BOOLEAN,
-    autoInit: Boolean(address && !mintingPaused),
-  });
+  // const {
+  //   data: allowlistEnabled,
+  //   fetch: refreshAllowlistState,
+  //   isLoading: allowlistStateLoading,
+  // } = useElvenScQuery<boolean>({
+  //   funcName: 'isAllowlistEnabled',
+  //   type: SCQueryType.BOOLEAN,
+  //   autoInit: Boolean(address && !mintingPaused),
+  // });
 
-  const {
-    data: tokensLimitPerAddressTotal,
-    fetch: refreshTokensLimitPerAddressTotal,
-    isLoading: isLoadingTokensLimitPerAddressTotal,
-  } = useElvenScQuery<number>({
-    funcName: 'getTokensLimitPerAddressTotal',
-    type: SCQueryType.NUMBER,
-    autoInit: Boolean(address && !mintingPaused),
-  });
+  // const {
+  //   data: tokensLimitPerAddressTotal,
+  //   fetch: refreshTokensLimitPerAddressTotal,
+  //   isLoading: isLoadingTokensLimitPerAddressTotal,
+  // } = useElvenScQuery<number>({
+  //   funcName: 'getTokensLimitPerAddressTotal',
+  //   type: SCQueryType.NUMBER,
+  //   autoInit: Boolean(address && !mintingPaused),
+  // });
 
-  const {
-    data: tokensLimitPerAddressPerDrop,
-    fetch: refreshTokensLimitPerAddressPerDrop,
-    isLoading: tokensLimitPerAddressPerDropLoading,
-  } = useElvenScQuery<number>({
-    funcName: 'getTokensLimitPerAddressPerDrop',
-    type: SCQueryType.NUMBER,
-    autoInit: Boolean(address && dropActive),
-  });
+  // const {
+  //   data: tokensLimitPerAddressPerDrop,
+  //   fetch: refreshTokensLimitPerAddressPerDrop,
+  //   isLoading: tokensLimitPerAddressPerDropLoading,
+  // } = useElvenScQuery<number>({
+  //   funcName: 'getTokensLimitPerAddressPerDrop',
+  //   type: SCQueryType.NUMBER,
+  //   autoInit: Boolean(address && dropActive),
+  // });
 
-  const {
-    data: totalTokensLeft,
-    fetch: refreshTotalTokensLeft,
-    isLoading: totalIsLoading,
-  } = useElvenScQuery<number>({
-    type: SCQueryType.NUMBER,
-    funcName: 'getTotalTokensLeft',
-  });
+  // const {
+  //   data: totalTokensLeft,
+  //   fetch: refreshTotalTokensLeft,
+  //   isLoading: totalIsLoading,
+  // } = useElvenScQuery<number>({
+  //   type: SCQueryType.NUMBER,
+  //   funcName: 'getTotalTokensLeft',
+  // });
 
-  const {
-    data: dropData,
-    fetch: refreshDropData,
-    isLoading: dropIsLoading,
-  } = useElvenScQuery<number>({
-    type: SCQueryType.NUMBER,
-    funcName: 'getDropTokensLeft',
-    autoInit: dropActive,
-  });
+  // const {
+  //   data: dropData,
+  //   fetch: refreshDropData,
+  //   isLoading: dropIsLoading,
+  // } = useElvenScQuery<number>({
+  //   type: SCQueryType.NUMBER,
+  //   funcName: 'getDropTokensLeft',
+  //   autoInit: dropActive,
+  // });
 
-  const {
-    data: mintedData,
-    fetch: refreshMintedData,
-    isLoading: mintedDataLoading,
-  } = useElvenScQuery<number>({
-    type: SCQueryType.NUMBER,
-    funcName: 'getMintedPerAddressTotal',
-    args: address ? [Address.fromBech32(address)?.hex()] : [],
-    autoInit: Boolean(address && !mintingPaused),
-  });
+  // const {
+  //   data: mintedData,
+  //   fetch: refreshMintedData,
+  //   isLoading: mintedDataLoading,
+  // } = useElvenScQuery<number>({
+  //   type: SCQueryType.NUMBER,
+  //   funcName: 'getMintedPerAddressTotal',
+  //   args: address ? [Address.fromBech32(address)?.hex()] : [],
+  //   autoInit: Boolean(address && !mintingPaused),
+  // });
 
-  const { data: mintedPerDropData, fetch: refreshMintedPerDropData } =
-    useElvenScQuery<number>({
-      type: SCQueryType.NUMBER,
-      funcName: 'getMintedPerAddressPerDrop',
-      args: address ? [Address.fromBech32(address)?.hex()] : [],
-      autoInit: Boolean(address && dropActive),
-    });
+  // const { data: mintedPerDropData, fetch: refreshMintedPerDropData } =
+  //   useElvenScQuery<number>({
+  //     type: SCQueryType.NUMBER,
+  //     funcName: 'getMintedPerAddressPerDrop',
+  //     args: address ? [Address.fromBech32(address)?.hex()] : [],
+  //     autoInit: Boolean(address && dropActive),
+  //   });
 
-  const {
-    data: allowlistCheckData,
-    fetch: refreshAllowlistCheckData,
-    isLoading: allowlistCheckLoading,
-  } = useElvenScQuery<number>({
-    type: SCQueryType.NUMBER,
-    funcName: 'getAllowlistAddressCheck',
-    args: address ? [Address.fromBech32(address)?.hex()] : [],
-    autoInit: Boolean(address && allowlistEnabled),
-  });
+  // const {
+  //   data: allowlistCheckData,
+  //   fetch: refreshAllowlistCheckData,
+  //   isLoading: allowlistCheckLoading,
+  // } = useElvenScQuery<number>({
+  //   type: SCQueryType.NUMBER,
+  //   funcName: 'getAllowlistAddressCheck',
+  //   args: address ? [Address.fromBech32(address)?.hex()] : [],
+  //   autoInit: Boolean(address && allowlistEnabled),
+  // });
 
-  useEffect(() => {
-    if (!mintingPaused) {
-      refreshTotalTokensLeft();
-    }
-  }, [mintingPaused]);
+  // useEffect(() => {
+  //   if (!mintingPaused) {
+  //     refreshTotalTokensLeft();
+  //   }
+  // }, [mintingPaused]);
 
-  useEffect(() => {
-    if (address) {
-      refreshMintedData();
-      refreshDropState();
-      refreshAllowlistState();
-      refreshTokensLimitPerAddressTotal();
-    }
-  }, [address]);
+  // useEffect(() => {
+  //   if (address) {
+  //     refreshMintedData();
+  //     refreshDropState();
+  //     refreshAllowlistState();
+  //     refreshTokensLimitPerAddressTotal();
+  //   }
+  // }, [address]);
 
-  useEffect(() => {
-    if (dropActive) {
-      refreshDropData();
-      refreshMintedPerDropData();
-      refreshTokensLimitPerAddressPerDrop();
-    }
-  }, [dropActive]);
+  // useEffect(() => {
+  //   if (dropActive) {
+  //     refreshDropData();
+  //     refreshMintedPerDropData();
+  //     refreshTokensLimitPerAddressPerDrop();
+  //   }
+  // }, [dropActive]);
 
-  useEffect(() => {
-    if (!allowlistStateLoading && allowlistEnabled) {
-      refreshAllowlistCheckData();
-    }
-  }, [allowlistEnabled]);
+  // useEffect(() => {
+  //   if (!allowlistStateLoading && allowlistEnabled) {
+  //     refreshAllowlistCheckData();
+  //   }
+  // }, [allowlistEnabled]);
 
-  const handleRefreshData = useCallback(() => {
-    refreshTotalTokensLeft();
-    refreshMintedData();
-    refreshMintedPerDropData();
-    refreshDropData();
-  }, [
-    refreshTotalTokensLeft,
-    refreshMintedData,
-    refreshMintedPerDropData,
-    refreshDropData,
-  ]);
+  // const handleRefreshData = useCallback(() => {
+  //   refreshTotalTokensLeft();
+  //   refreshMintedData();
+  //   refreshMintedPerDropData();
+  //   refreshDropData();
+  // }, [
+  //   refreshTotalTokensLeft,
+  //   refreshMintedData,
+  //   refreshMintedPerDropData,
+  //   refreshDropData,
+  // ]);
 
-  const getLeftToMintForUser = useCallback(() => {
-    let leftPerDrop = 0;
-    let leftInTotal = 0;
+  // const getLeftToMintForUser = useCallback(() => {
+  //   let leftPerDrop = 0;
+  //   let leftInTotal = 0;
 
-    if (allowlistEnabled && allowlistCheckData === 0) {
-      return 0;
-    }
+  //   if (allowlistEnabled && allowlistCheckData === 0) {
+  //     return 0;
+  //   }
 
-    if (
-      !Number.isNaN(mintedPerDropData) &&
-      !Number.isNaN(tokensLimitPerAddressPerDrop)
-    ) {
-      leftPerDrop = tokensLimitPerAddressPerDrop - mintedPerDropData;
-    }
-    if (
-      !Number.isNaN(mintedData) &&
-      !Number.isNaN(tokensLimitPerAddressTotal)
-    ) {
-      leftInTotal = tokensLimitPerAddressTotal - mintedData;
-    }
-    if (!dropActive || leftPerDrop > leftInTotal) {
-      return leftInTotal;
-    }
-    return leftPerDrop;
-  }, [
-    allowlistEnabled,
-    allowlistCheckData,
-    mintedPerDropData,
-    tokensLimitPerAddressPerDrop,
-    mintedData,
-    tokensLimitPerAddressTotal,
-    dropActive,
-  ]);
+  //   if (
+  //     !Number.isNaN(mintedPerDropData) &&
+  //     !Number.isNaN(tokensLimitPerAddressPerDrop)
+  //   ) {
+  //     leftPerDrop = tokensLimitPerAddressPerDrop - mintedPerDropData;
+  //   }
+  //   if (
+  //     !Number.isNaN(mintedData) &&
+  //     !Number.isNaN(tokensLimitPerAddressTotal)
+  //   ) {
+  //     leftInTotal = tokensLimitPerAddressTotal - mintedData;
+  //   }
+  //   if (!dropActive || leftPerDrop > leftInTotal) {
+  //     return leftInTotal;
+  //   }
+  //   return leftPerDrop;
+  // }, [
+  //   allowlistEnabled,
+  //   allowlistCheckData,
+  //   mintedPerDropData,
+  //   tokensLimitPerAddressPerDrop,
+  //   mintedData,
+  //   tokensLimitPerAddressTotal,
+  //   dropActive,
+  // ]);
 
-  const isContentCentered = useBreakpointValue({ base: true, md: false });
+  // const isContentCentered = useBreakpointValue({ base: true, md: false });
 
-  const tokensLeftPerUser = getLeftToMintForUser();
+  // const tokensLeftPerUser = getLeftToMintForUser();
 
   return (
     <Box width="100%">
